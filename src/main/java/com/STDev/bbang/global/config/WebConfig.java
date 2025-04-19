f2c3ebaf2d4aff5b81931682a78292c282097640
혜민 <alddlv81@gmail.com>
2025-04-19 19:33:02 +0900

    feat: url 여러개 하기 위해 @ConfigurationProperties 방법 사용

--- src/main/java/com/STDev/bbang/global/config/WebConfig.java
+++ src/main/java/com/STDev/bbang/global/config/WebConfig.java
@@ -1,22 +1,22 @@
 package com.STDev.bbang.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("${cors.allowed-origin}")
-    private List<String> allowedOrigins;
+    private final CorsProperties corsProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins.toArray(new String[0]))
+                .allowedOrigins(corsProperties.getAllowedOrigin().toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
