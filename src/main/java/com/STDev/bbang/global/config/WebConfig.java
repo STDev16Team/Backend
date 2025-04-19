ad8ffe233e4c971557f8737ef638a9e458f1813c
혜민 <alddlv81@gmail.com>
2025-04-19 20:24:28 +0900

    fix: 해결 후 url 두개로 수정

--- src/main/java/com/STDev/bbang/global/config/WebConfig.java
+++ src/main/java/com/STDev/bbang/global/config/WebConfig.java
@@ -1,20 +1,17 @@
 package com.STDev.bbang.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Value("${cors.allowed-origin}")
-    private String allowedOrigin;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigin)
+                .allowedOrigins("http://localhost:5173", "https://54.180.191.123:8443")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
