04c6ca0ba3bdb1432ee3afd76a0d5bc596c8ca7c
혜민 <alddlv81@gmail.com>
2025-04-19 19:24:41 +0900

    feat: url 추가

--- src/main/java/com/STDev/bbang/global/config/WebConfig.java
+++ src/main/java/com/STDev/bbang/global/config/WebConfig.java
@@ -5,16 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${cors.allowed-origin}")
-    private String allowedOrigin;
+    private List<String> allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigin)
+                .allowedOrigins(allowedOrigins.toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
