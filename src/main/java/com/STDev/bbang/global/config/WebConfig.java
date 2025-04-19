503eac15768ea3857d423ccaeebe420bd1d118cf
혜민 <alddlv81@gmail.com>
2025-04-19 19:39:33 +0900

    feat: 링크 넣는 걸로 수정

--- src/main/java/com/STDev/bbang/global/config/WebConfig.java
+++ src/main/java/com/STDev/bbang/global/config/WebConfig.java
@@ -1,22 +1,17 @@
 package com.STDev.bbang.global.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
-    private final CorsProperties corsProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(corsProperties.getAllowedOrigin().toArray(new String[0]))
+                .allowedOrigins("http://localhost:5173", "https://54.180.191.123:8443")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
