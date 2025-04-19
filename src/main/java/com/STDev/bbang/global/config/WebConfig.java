069f3352136abc2d26baa3e410200ba7b424d272
혜민 <alddlv81@gmail.com>
2025-04-20 00:06:28 +0900

    chore: url 추가

--- src/main/java/com/STDev/bbang/global/config/WebConfig.java
+++ src/main/java/com/STDev/bbang/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://54.180.191.123:8443")
+                .allowedOrigins("http://localhost:5173", "https://54.180.191.123:8443", "http://54.180.191.123:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
